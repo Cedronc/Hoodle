ea5a93cfe84c5ebb3cef82a32bb940018d54a024
Cedric M. <cedricmrtl@pm.me>
2026-05-29 21:57:16 +0200

    1st attempt at parser implemented in TCP

--- Parser.hs
+++ Parser.hs
@@ -1,7 +1,123 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Parser where
 
-import Text.Parsec hiding (State)
-import Text.Parsec.String
-import Control.Monad
+import Text.Parsec
+import qualified Text.Parsec
+import Text.Parsec.String (Parser)
+-- import Control.Applicative ((<|>), many, some)
+import Data.Char (isAlphaNum)
 
+--  TYPES
 
+type Token    = String
+type Login    = (Token, Token)  -- LOGIN → TOKEN : TOKEN
+type Time     = String
+type Slot     = (Time, Time)    -- SLOT → TIME / TIME
+type Slots    = [Slot]
+type Hoodle   = Slots           -- HOODLE → [ SLOTS ]
+type Hoodles  = [(Token, Slot)] -- HOODLES → TOKEN : SLOT , HOODLES
+type Schedule = Hoodles         -- SCHEDULE → { HOODLES }
+
+data Request
+  = AddUser Login Token
+  | GetHoodle Token
+  | ChangePassword Login Token
+  | AddHoodle Login Token Hoodle
+  | EditHoodle Login Token Hoodle
+  | RemoveHoodle Login Token
+  | Register Login Token
+  | Unregister Login Token
+  | Toggle Login Token Slot
+  | OptimalSchedule Login
+  deriving (Show, Eq)
+
+data Response
+  = WrongLogin
+  | OkToken Token
+  | OkHoodle Hoodle
+  | OkSchedule Schedule
+  | NotPermitted
+  | InvalidHoodle
+  | NoSuchId
+  | NoSuchSlot
+  | NotRegistered
+  | AlreadyRegistered
+  | NoPossibleSchedule
+  deriving (Show, Eq)
+
+-- LEXER HELPERS
+
+lexeme :: Parser a -> Parser a
+lexeme p = p <* spaces
+
+symbol :: Char -> Parser Char
+symbol c = lexeme (char c)
+
+stringL :: String -> Parser String
+stringL s = lexeme (string s)
+
+parseToken :: Parser String
+parseToken = lexeme $ many1 (alphaNum <|> char '-' <|> char '_')
+
+timeToken :: Parser Time
+timeToken = lexeme $ many1 (alphaNum <|> char ':' <|> char '-' <|> char '+' <|> char 'T')
+
+-- Fix all single-character parsers:
+lbrack, rbrack, lcurly, rcurly, comma, colon, slash :: Parser Char
+lbrack = symbol '['
+rbrack = symbol ']'
+lcurly = symbol '{'
+rcurly = symbol '}'
+comma  = symbol ','
+colon  = symbol ':'
+slash  = symbol '/'
+
+-- BASIC PARSERS
+-- `sepEndBy` allows trailing parsing thingies
+
+parseLogin :: Parser Login
+parseLogin = (,) <$> parseToken <*> (colon *> parseToken)
+
+parseSlot :: Parser Slot
+parseSlot = (,) <$> timeToken <*> (slash *> timeToken)
+
+parseSlots :: Parser Slots
+parseSlots = parseSlot `sepEndBy` comma
+
+parseHoodle :: Parser Hoodle
+parseHoodle = lbrack *> parseSlots <* rbrack
+
+parseHoodlesEntry :: Parser (Token, Slot)
+parseHoodlesEntry = (,) <$> parseToken <*> (colon *> parseSlot)
+
+parseHoodles :: Parser Hoodles
+parseHoodles = parseHoodlesEntry `sepEndBy` comma
+
+parseSchedule :: Parser Schedule
+parseSchedule = lcurly *> parseHoodles <* rcurly
+
+-- REQUEST PARSER
+
+parseRequest :: Parser Request
+
+parseRequest = choice
+  [ try (AddUser <$ stringL "add-user" <*> parseLogin <*> parseToken)
+  , try (AddHoodle <$ stringL "add-hoodle" <*> parseLogin <*> parseToken <*> parseHoodle)
+  , try (ChangePassword <$ stringL "change-password" <*> parseLogin <*> parseToken)
+  , try (EditHoodle <$ stringL "edit-hoodle" <*> parseLogin <*> parseToken <*> parseHoodle)
+  , try (RemoveHoodle <$ stringL "remove-hoodle" <*> parseLogin <*> parseToken)
+  , try (GetHoodle <$ stringL "get-hoodle" <*> parseToken)
+  , try (Register <$ stringL "register" <*> parseLogin <*> parseToken)
+  , try (Unregister <$ stringL "unregister" <*> parseLogin <*> parseToken)
+  , try (Toggle <$ stringL "toggle" <*> parseLogin <*> parseToken <*> parseSlot)
+  , try (OptimalSchedule <$ stringL "optimal-schedule" <*> parseLogin)
+  ]
+
+-- TOP-LEVEL PARSER
+
+parseProtocolMessage :: String -> Either ParseError Request
+parseProtocolMessage input =
+  case parse parseRequest "<input>" input of
+    Right r -> Right r
+    Left err -> Left err
--- TCP.hs
+++ TCP.hs
@@ -1,27 +1,40 @@
 module TCP where
 
-import Network.Simple.TCP (HostName, HostPreference(Host), ServiceName, connect, serve, Socket, accept, send, recv)
-import Hoodle
-import Pool
-import Data.Time (LocalTime)
-import LazyHoodle
 import Control.Monad (unless, when)
-
-import qualified Data.ByteString.Char8 as B
-
+import Data.ByteString.Char8 qualified as B
+import Data.Time (LocalTime)
+import Hoodle
+import LazyHoodle
+import Network.Simple.TCP (HostName, HostPreference (Host), ServiceName, Socket, accept, connect, recv, send, serve)
+import Parser
+import Pool
 
 client = connect "localhost" "4000" $ \(sock, addr) -> do
   putStrLn $ "Connection established to " ++ show addr
   action <- getLine
   send sock (B.pack action)
-  -- Now you may use connectionSocket as you please within this scope,
-  -- possibly using recv and send to interact with the remote end
+
+-- Now you may use connectionSocket as you please within this scope,
+-- possibly using recv and send to interact with the remote end
 
 server :: IO ()
 server = serve (Host "127.0.0.1") "4000" $ \(sock, addr) -> do
-    putStrLn $ "Connection from: " ++ show addr
-    msg <- recv sock 255
-    case msg of
-        Just bs -> do
-            putStrLn $ "Received: " ++ show bs
-        Nothing -> putStrLn "Connection closed"
+  putStrLn $ "Connection from: " ++ show addr
+  msg <- recv sock 255
+  case msg of
+    Just bs -> case parseProtocolMessage (B.unpack bs) of
+      Right request -> print $ generateResponse request
+      Left err -> print err
+    Nothing -> putStrLn "Connection closed"
+
+generateResponse :: Request -> Response
+generateResponse (ChangePassword _ _) = OkToken "password-updated"
+generateResponse (AddUser _ _) = OkToken "user-added"
+generateResponse (GetHoodle _ ) = OkHoodle [] -- Empty hoodle (replace with real data)
+generateResponse (AddHoodle _ _ _) = OkToken "hoodle-added"
+generateResponse (EditHoodle _ _ _) = OkToken "hoodle-edited"
+generateResponse (RemoveHoodle _ _) = OkToken "hoodle-removed"
+generateResponse (Register _ _) = OkToken "registration-token"
+generateResponse (Unregister _ _) = OkToken "unregistered"
+generateResponse (Toggle _ _ _) = OkToken "slot-toggled"
+generateResponse (OptimalSchedule _) = OkSchedule [] -- Empty schedule (replace with real data)
