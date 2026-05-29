1eaa95bd1de491b3bac027dc5c94a5d11efbe82a
Cedric M. <cedricmrtl@pm.me>
2026-05-29 16:01:39 +0200

    euh finishing

--- TCP.hs
+++ TCP.hs
@@ -1,27 +1,28 @@
 module TCP where
 
+import Control.Concurrent.STM
 import Control.Monad (unless, when)
+import Control.Monad.Except
+import Control.Monad.Reader
 import Data.ByteString.Char8 qualified as B
 import Data.Time (LocalTime)
-import Pool
 import Hoodle
 import LazyHoodle
 import Network.Simple.TCP (HostName, HostPreference (Host), ServiceName, Socket, accept, connect, recv, send, serve)
 import Parser
-import Control.Monad.Reader
-import Control.Concurrent.STM
-import Control.Monad.Except
+import Pool
 
+-- Move connect to connectSock to manually close connections
 client = connect "localhost" "4000" $ \(sock, addr) -> do
   putStrLn $ "Connection established to " ++ show addr
+  putStr "<== "
   action <- getLine
   send sock (B.pack action)
   msg <- recv sock 255
   case msg of
-    Just val -> putStrLn (B.unpack val)
-
--- Now you may use connectionSocket as you please within this scope,
--- possibly using recv and send to interact with the remote end
+    Just val -> do
+      putStrLn $ "==> " ++ show val
+    Nothing -> putStrLn "Connection closed"
 
 server :: IO ()
 server = do
@@ -37,15 +38,15 @@ server = do
     msg <- recv sock 255
 
     case msg of
-          Just bs -> case parseProtocolMessage (B.unpack bs) of
-            Right request -> do
-              -- Run with BOTH poolVar and dbVar
-              result <- runAppAtomically (processRequest request) appState
-              case result of
-                Right response -> send sock (B.pack (show response))
-                Left err -> send sock (B.pack (show err))
-            Left err -> send sock (B.pack "error")
-          Nothing -> putStrLn "Connection closed"
+      Just bs -> case parseProtocolMessage (B.unpack bs) of
+        Right request -> do
+          -- Run with BOTH poolVar and dbVar
+          result <- runAppAtomically (processRequest request) appState
+          case result of
+            Right response -> send sock (B.pack (show response))
+            Left err -> send sock (B.pack (show err))
+        Left err -> send sock (B.pack "error")
+      Nothing -> putStrLn "Connection closed"
 
 processRequest :: Request -> AppMonad Response
 processRequest (ChangePassword login newPass) = changePassword login newPass
@@ -59,15 +60,13 @@ processRequest (AddHoodle login token hoodle) = do
       lift . lift $ modifyTVar poolVar (snd . addToPool hoodle)
       return (OkToken "hoodle-added")
     else return WrongLogin
-
-
 processRequest req = return (OkToken "not-implemented")
 
 changePassword :: Login -> Token -> AppMonad Response
 changePassword login newPass = do
   (tvarDB, _) <- ask
   -- first lifted to (ExceptT Response STM) then lifted to (ReaderT AppState)
-  db <- lift . lift $ readTVar tvarDB 
+  db <- lift . lift $ readTVar tvarDB
   if validLogin login db
     then lift . lift $ modifyTVar tvarDB (addUser (fst login) newPass) >> return (OkToken "changed")
     else throwError WrongLogin
